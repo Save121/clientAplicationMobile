f87ac947a8af4fc87b05fa8c7bfd6a0aa8d2540d
Andy Avelar <andysave121@gmail.com>
2022-05-24 14:47:07 -0600

    Creando tarjeta para cada canal y agregarla con botón

--- app/src/main/java/com/example/clientaplicationmobile/MainActivity.kt
+++ app/src/main/java/com/example/clientaplicationmobile/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.clientaplicationmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val addChannelButton: FloatingActionButton = findViewById(R.id.addChannelButton)
+
+            addChannelButton.setOnClickListener {addNewChannel()}
+
+        }
+
+    private fun addNewChannel() {
+        this.appendChannel("And")
     }
+
+    private fun appendChannel(name:String = "undefined",nameCreator:String = "anonymous" ){
+        val channelCard: View = layoutInflater.inflate(R.layout.channel_card, null)
+       val channelName: TextView = channelCard.findViewById(R.id.channelName)
+        channelName.text = name
+        val channelCreator: TextView = channelCard.findViewById(R.id.channelCreator)
+        channelCreator.text = nameCreator
+
+        val channelList: LinearLayout = findViewById(R.id.channelList)
+
+        channelList.addView(channelCard)
+    }
+
+
 }
